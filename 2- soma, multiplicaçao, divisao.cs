efdaae704a21c8aa38b16a559e56fefd90fe1528
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-09-09 01:18:11 -0300

    2- soma, sub, mul, divisao

---
+++ 2- soma, multiplicaçao, divisao.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, soma, sub, mult, div;
+
+            Console.WriteLine("Digite n1:");
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite n2:");
+            n2 = int.Parse(Console.ReadLine());
+
+            soma = n1 + n2;
+            sub = n1 - n2;
+            div = n1 / n2;
+            mult = n1 * n2;
+
+            Console.WriteLine(soma);
+            Console.WriteLine(sub);
+            Console.WriteLine(div);
+            Console.WriteLine(mult);
+
+                
+
+
+        }
+    }
+}
