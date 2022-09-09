2319d9869e33fbfae8a9e0ff35c0bdaf7e2cfab3
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-09-09 01:18:47 -0300

    3- 20% do salario

---
+++ 3- 20% de salario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            double salfx, totalvend, salfin;
+
+            Console.WriteLine("Digite seu nome");
+            name = Console.ReadLine();
+
+            Console.WriteLine("seu salario fixo: ");
+            salfx = double.Parse(Console.ReadLine());
+            Console.WriteLine("total de vendas: ");
+            totalvend = double.Parse(Console.ReadLine());
+            Console.WriteLine("Seu salario final é : ");
+            salfin = salfx * 20/ 1000 + salfx;
+            
+            Console.WriteLine(salfin);
+
+
+        }
+    }
+}
