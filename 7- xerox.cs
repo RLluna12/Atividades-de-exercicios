e1fe5211d8dd25cb8490692e9ff5f8b4800d4dd8
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-09-09 01:47:20 -0300

    xerox

---
+++ 7- xerox.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double folhas;
+            Console.WriteLine("Numero de folhas:");
+            folhas = double.Parse(Console.ReadLine());
+
+            if (folhas > 200)
+            {
+                Console.WriteLine(folhas * 0.30);
+            }
+            else
+            {
+                Console.WriteLine(folhas * 0.50);
+            }
+
+        }
+    }
+}
