2bb5c7bab07448489bd97fe828877fb260dd1217
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-09-08 23:43:41 -0300

    Area do triangulo

---
+++ 1- area do triangulo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_lista_de_exercicios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int area, bas, alt;
+            Console.Write("Digite o valor da base: ");
+            bas = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor da altura: ");
+            alt = int.Parse(Console.ReadLine());
+
+            area = bas * alt / 2;
+
+            Console.WriteLine("a area do quadrado é: "+ area);
+            
+
+
+
+
+
+        }
+    }
+}
