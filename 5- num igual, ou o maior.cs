792337e68c50a019633ef3d91cd71e55f5729fed
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-09-09 01:19:50 -0300

    5- num igual ou maior

---
+++ 5- num igual, ou o maior.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nun1, nun2;
+
+            Console.WriteLine("digite numero 1 : ");
+            nun1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("digite numero 2 : ");
+            nun2 = int.Parse(Console.ReadLine());
+
+            if (nun1 == nun2)
+            {
+                Console.WriteLine("sao iguais");
+
+            }
+            else if (nun1 > nun2)
+            {
+                Console.WriteLine("{0} maior", nun1);
+            }
+            else if (nun1 < nun2)
+            {
+                Console.WriteLine("{0} maior", nun2);
+            }
+
+            
+        }
+    }
+}
