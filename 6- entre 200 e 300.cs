9d5c1cba648b49ddd9d918fb8f0ba32c74973b46
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-09-09 01:20:29 -0300

    6- entre 200 e 300

---
+++ 6- entre 200 e 300.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1;
+            Console.WriteLine("Digite um Numero: ");
+            n1 = int.Parse(Console.ReadLine());
+
+            if (n1 >= 200 && n1 <= 300)
+            {
+                Console.WriteLine("Entre 200 e 300");
+            }
+            else if (n1 < 200 || n1 > 300)
+            { 
+                Console.WriteLine("nao esta entra 200 e 300"); 
+            }
+           
+
+        }
+    }
+}
