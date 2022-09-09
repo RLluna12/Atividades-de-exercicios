f260df56ddfb163b26207a0bdb08e438f61845a6
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-09-09 01:19:10 -0300

    4- nota

---
+++ 4 - nota.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String name;
+            double p1, p2, p3, media;
+
+            Console.WriteLine("digite seu nome: ");
+            name = Console.ReadLine();
+            p1 = 2;
+            p2 = 4;
+            p3 = 6;
+            media = (p1 + p2 + p3) / 3;
+
+            Console.WriteLine(name + " sua nota é : " + media);
+
+            
+
+
+
+
+ 
+
+        }
+    }
+}
