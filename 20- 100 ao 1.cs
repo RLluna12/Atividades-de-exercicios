ae4ea762a54529004bb589876df256e238805ae3
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-09-15 00:24:32 -0300

    20- 100 ao 1

---
+++ 20- 100 ao 1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            i = 100;
+            while (i>0)
+            {
+                Console.WriteLine(i);
+                i--;
+            }
+        }
+    }
+}
