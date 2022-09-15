072164f9920f97bb34ed96d852dd9a593af3566f
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-09-15 00:23:34 -0300

    17- aluno aprovado ou não + media

---
+++ 17- aluno aprovado ou nao + media.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, n3, n4, media;
+            Console.WriteLine("Digite a nota 1: ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a nota 2: ");
+            n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a nota 3: ");
+            n3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a nota 4: ");
+            n4 = int.Parse(Console.ReadLine());
+            media = (n1 + n2 + n3 + n4) / 4;
+            
+            if (media < 5)
+            {
+                Console.WriteLine("Reprovado "+media);
+            }
+            else
+            {
+                Console.WriteLine("Aprovado " + media);
+            }
+
+        }
+    }
+}
