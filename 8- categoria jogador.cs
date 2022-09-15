4e7d53aff008547bc4793d0baf23350e6de5d934
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-09-15 00:20:18 -0300

    8- categoria jogador

---
+++ 8- categoria jogador.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            Console.WriteLine("Digite sua idade: ");
+            idade = int.Parse(Console.ReadLine());
+
+            if (idade <=13)
+            {
+                Console.WriteLine("Sua categoria é infantil");
+            }
+            else if (idade <= 17)
+            {
+                Console.WriteLine("Sua categoria é juvenil");
+            }
+            else
+            {
+                Console.WriteLine("Sua categoria é sênior");
+            }
+        }
+    }
+}
