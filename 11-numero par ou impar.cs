148e7bf0da868c9670a3766d714a07be74ea05aa
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-09-15 00:21:29 -0300

    11-numero par ou impar

---
+++ 11-numero par ou impar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1;
+            Console.Write("Digite o primeiro numero: ");
+            n1 = int.Parse(Console.ReadLine());
+            
+            if (n1%2==0)
+            {
+                Console.WriteLine("par");
+            }
+            else
+            {
+                Console.WriteLine("impar");
+            }
+            
+        }
+    }
+}
