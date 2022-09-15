e3f0bf0e6bd2b59c7756894b54e5818f016a5724
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-09-15 00:21:49 -0300

    12- Nome e media do aluno

---
+++ 12- Nome e media do aluno.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String name;
+            Console.WriteLine("Digite seu nome: ");
+            name = Console.ReadLine();
+           double n1, n2, n3, media;
+            Console.WriteLine("Digite sua nota 1: ");
+            n1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite sua nota 2: ");
+            n2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite sua nota 3: ");
+            n3 = double.Parse(Console.ReadLine());
+
+            media = (n1 + n2 + n3) / 3;
+            Console.WriteLine(name +" sua media é: " + media);
+
+
+
+
+
+        }
+    }
+}
