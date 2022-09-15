6f7106fbc0898624437e13be2c7d9ab77bcb9e50
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-09-15 00:24:14 -0300

    19-1 ao 10

---
+++ 19-1 ao 10.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            i=1;
+            while (i<11)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
