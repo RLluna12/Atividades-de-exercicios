8902ac63c62b37e89cf97c9bbbc96457b7af1556
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-09-15 00:22:33 -0300

    14- Celsius em Fahrenheit

---
+++ 14- Celsius em Fahrenheit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double f, c;
+            Console.WriteLine("temperatura em grau Celsius: ");
+            c = double.Parse(Console.ReadLine());
+
+            f = (9 * c + 160) / 5;
+            Console.WriteLine("a conversao de grau Celsius em grau Fahrenheit é : "+ f);
+
+               
+            
+
+
+        }
+    }
+}
