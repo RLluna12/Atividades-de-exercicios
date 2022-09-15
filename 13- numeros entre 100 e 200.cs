77870a03d66eabe02f72f6a61e863cc3e924c956
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-09-15 00:22:14 -0300

    13- números entre 100 e 200

---
+++ 13- numeros entre 100 e 200.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1;
+            Console.WriteLine("Digite um numero: ");
+            n1= int.Parse(Console.ReadLine());
+
+            if(n1>=100 && n1<=200)
+            {
+                Console.WriteLine("Esta entra 100 e 200");
+            }
+            else
+            {
+                Console.WriteLine("Não esta entre 100 e 200");
+            }
+        }
+    }
+}
