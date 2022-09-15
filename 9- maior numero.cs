5bd846b1a73dedcb4d46ae8fa26e57089e211e27
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-09-15 00:20:40 -0300

    9- maior numero

---
+++ 9- maior numero.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+            Console.WriteLine("Digite o n1: ");
+            n1 = int.Parse(Console.ReadLine()); 
+            Console.WriteLine("Digite o n2: ");
+            n2 = int.Parse(Console.ReadLine());
+
+            if (n1 > n2)
+            {
+                Console.WriteLine("{0} o maior", n1);
+            }
+            else
+            {
+                Console.WriteLine("{0} o maior", n2);
+            }
+        }
+    }
+}
